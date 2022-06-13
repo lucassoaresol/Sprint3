d16d7864f4bc0c3a8f96788fa276ffe2f28a6e22
Lucas <lucassoares2007@hotmail.com>
2022-06-13 18:10:15 -0300

    adicionando primeira atividade

---
+++ S3_08_Atividade_Javascript_Praticando_variáveis/src/js/index.js
@@ -0,0 +1,29 @@
+//🎲 Exercício 1
+
+let nome = "Lucas"
+
+console.log("Olá, eu sou o",nome)
+
+//🎲 Exercício 2
+
+let sobrenome = "Soares"
+
+console.log("Olá, eu me chamo",nome,sobrenome)
+
+//🎲 Exercício 3
+
+let primeiroNome = "Lucas"
+let segundoNome = "Soares"
+let idade = 25
+
+console.log("Olá, meu primeiro nome é",primeiroNome,"meu segundo nome é",segundoNome+".","E eu tenho",idade,"anos.")
+
+//🎲 Exercício 4
+
+let preco1 = 10
+let preco2 = 20
+let precoTotal = preco1 + preco2
+
+console.log("Comprei uma xícara por",preco1,"reais, e dois copos por",preco2,"reais. No total, gastei",precoTotal,"reais.")
+
+//
